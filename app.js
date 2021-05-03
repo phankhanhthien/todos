da49d0d4c10a7911fe6c8d3ae6c7e964e385786c
khanhthien <phankhanhthien1998@gmail.com>
2021-05-04 04:46:29 +0700

    first commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/Todos', todos);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next) {gi
   
   next(createError(404));
 });
